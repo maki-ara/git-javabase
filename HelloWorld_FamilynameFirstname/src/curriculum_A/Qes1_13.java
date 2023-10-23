09d35fbec8696ac18bf8e96ea3adf64ecc9ed729
maki-ara <muomuomuo.54@gmail.com>
2023-10-24 00:24:29 +0900

    試しにコミット

---
+++ HelloWorld_FamilynameFirstname/src/curriculum_A/Qes1_13.java
@@ -0,0 +1,33 @@
+package curriculum_A;
+
+public class Qes1_13 {
+    public static void main(String[] args) {
+        // バイト型
+        byte byteVar;
+
+        // 短整数型
+        short shortVar;
+
+        // 整数型
+        int intVar;
+
+        // 長整数型
+        long longVar;
+
+        // 単精度浮動小数点数型
+        float floatVar;
+
+        // 倍精度浮動小数点数型
+        double doubleVar;
+
+        // 文字型
+        char charVar;
+
+        // 文字列型 (String)
+        String stringVar;
+
+        // ブーリアン型
+        boolean booleanVar;
+    }
+}
+
