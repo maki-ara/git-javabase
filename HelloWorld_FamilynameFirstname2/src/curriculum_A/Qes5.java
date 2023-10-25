ee3b284b026fdb6d42dc6a9b4463ba233e537a72
maki-ara <muomuomuo.54@gmail.com>
2023-10-25 23:50:51 +0900

    Qes5 解答

---
+++ HelloWorld_FamilynameFirstname2/src/curriculum_A/Qes5.java
@@ -0,0 +1,12 @@
+package curriculum_A;
+
+public class Qes5 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		String num = "20";
+		int num1 = 23;
+		System.out.println("ハローJAVA" + (Integer.parseInt(num) + num1));
+	}
+
+}
