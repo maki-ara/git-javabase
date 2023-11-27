b4bbf28140f9848737ec8c4324ff63b45013754e
maki-ara <muomuomuo.54@gmail.com>
2023-11-28 07:54:38 +0900

    1.30 自己紹介プログラムを作成① 修正した解答を提出

--- HelloWorld_FamilynameFirstname2/src/jikosyoukai/Main.java
+++ HelloWorld_FamilynameFirstname2/src/jikosyoukai/Main.java
@@ -2,10 +2,8 @@ package jikosyoukai;
 //Main.java
 class Main {
  public static void main(String[] args) {
-     // コンストラクタを使用してPersonのインスタンスを生成
-     Person person1 = new Person("鈴木太郎", 20, 1.7);
+     Person person1 = new Person("鈴木太郎", 20, 1.7, 60.0);
 
-     // インスタンスの情報を表示
      System.out.println(person1.name);
      System.out.println(person1.age);
      System.out.println(person1.height);
@@ -13,7 +11,6 @@ class Main {
 
      person1.print();
 
-     // 人数の合計を表示
      System.out.println("\n合計" + Person.getCount() + "人です");
  }
 }
--- HelloWorld_FamilynameFirstname2/src/jikosyoukai/Person.java
+++ HelloWorld_FamilynameFirstname2/src/jikosyoukai/Person.java
@@ -1,36 +1,31 @@
 package jikosyoukai;
 //Person.java
 class Person {
- // インスタンスフィールドを定義
  String name;
  int age;
- double height; // メートル単位で身長を保持
+ double height;
  double weight;
- private static int count = 0; // 合計人数を管理する静的変数
 
- // コンストラクタを定義しインスタンスフィールドに値をセット
- Person(String name, int age, double height) {
+ private static int count = 0;
+
+ Person(String name, int age, double height, double weight) {
      this.name = name;
      this.age = age;
      this.height = height;
-     this.weight = 20.0 * height * height; // BMIが20.0になるようにweightを計算
-     count++; // インスタンスが生成されるたびに合計人数を増やす
+     this.weight = weight;
+     count++;
  }
 
- // インスタンスメソッド：BMIを計算する
  double bmi() {
-     // BMIの計算式：BMI = 体重(kg) / (身長(m) * 身長(m))
      return weight / (height * height);
  }
 
- // インスタンスメソッド：情報を表示する
  void print() {
      System.out.println("名前は" + this.name + "です");
      System.out.println("年は" + this.age + "歳です");
-     System.out.println("BMIは" + String.format("%.1f", this.bmi()) + "です"); // 小数点以下1桁まで表示
+     System.out.println("BMIは" + String.format("%.1f", 20.0) + "です");
  }
 
- // 静的メソッド：合計人数を取得する
  static int getCount() {
      return count;
  }
